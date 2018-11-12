63f7ed4978c69e8192e4f288db243e7e04fa3e0f
Xur <wgerraughty3749@gmail.com>
2018-11-12 07:24:21 -0500

    Basic UI Layout, Prep Work on: Saving/Loading files, localization, presets, and item select. Immediate To-Do: Figure out how to do save/load (write bytes or decrypt?) and mark down every purchasable item + their ID.

---
+++ MHW-Shop-Editor/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MHW_Shop_Editor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void openFile(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                FileName = "shopList",
+                DefaultExt = ".slt",
+                Filter = "Shop List file | *.slt",
+                InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory
+            };
+
+            if (dlg.ShowDialog() == true)
+            {
+                string filename = dlg.FileName;
+            }
+        }
+
+        private void saveFile(object sender, RoutedEventArgs e)
+        {
+            System.IO.Stream fs;
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog()
+            {
+                FileName = "shopList",
+                DefaultExt = ".slt",
+                Filter = "Shop List file | *.slt",
+                InitialDirectory = System.AppDomain.CurrentDomain.BaseDirectory
+            };
+
+
+            if (dlg.ShowDialog() == true)
+            {
+                if ((fs = dlg.OpenFile()) != null)
+                {
+                    // Code to write the stream goes here.
+                    fs.Close();
+                }
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ResourceDictionary dict = new ResourceDictionary();
+
+            switch (((sender as ComboBox).SelectedItem as ComboBoxItem).Tag.ToString())
+            {
+                case "en-US":
+                    dict.Source = new Uri("Lang.en-US.xaml", UriKind.Relative);
+                    break;
+                case "es-ES":
+                    dict.Source = new Uri("Lang.es-ES.xaml", UriKind.Relative);
+                    break;
+                default:
+                    break;
+            }
+            this.Resources.MergedDictionaries.Add(dict);
+        }
+
+    }
+}
