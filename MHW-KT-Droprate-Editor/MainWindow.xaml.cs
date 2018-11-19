5e8ac5f7dea7d0aacc23d6abc3d4a896764a9e90
Jason Yu <jasonycw1992@gmail.com>
2018-11-19 21:30:59 +0800

    Refactor and add save function

--- MHW-KT-Droprate-Editor/MainWindow.xaml.cs
+++ MHW-KT-Droprate-Editor/MainWindow.xaml.cs
@@ -1,11 +1,18 @@
-﻿using MhwKtDroprateEditor.Models;
+﻿using MhwKtDroprateEditor.Annotations;
+using MhwKtDroprateEditor.Models;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Xceed.Wpf.Toolkit;
+using MessageBox = System.Windows.MessageBox;
 
 namespace MhwKtDroprateEditor
 {
@@ -16,7 +23,7 @@ namespace MhwKtDroprateEditor
     {
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public bool rendered = false;
+        public bool Rendered = false;
 
         public Droprate BrownDroprate;
         public Droprate SilverDroprate;
@@ -33,7 +40,7 @@ namespace MhwKtDroprateEditor
         {
             InitializeProperties();
             InitializeComponent();
-            rendered = true;
+            Rendered = true;
             PresetDroprate("Default.json");
         }
 
@@ -55,25 +62,25 @@ namespace MhwKtDroprateEditor
 
         private void Render()
         {
-            BrownR6Pre.Value = BrownDroprate.R6GoldPrefix;
-            BrownR6Post.Value = BrownDroprate.R6GoldPostfix;
-            BrownR7.Value = BrownDroprate.R7;
-            BrownR8.Value = BrownDroprate.R8;
-            BrownTotal.Text = $"{(BrownDroprate.R6GoldPrefix + BrownDroprate.R6GoldPostfix + BrownDroprate.R7 + BrownDroprate.R8) * 100}%";
+            Render(BrownDroprate, BrownR6Pre, BrownR6Post, BrownR7, BrownR8, BrownTotal);
+            Render(SilverDroprate, SilverR6Pre, SilverR6Post, SilverR7, SilverR8, SilverTotal);
+            Render(GoldDroprate, GoldR6Pre, GoldR6Post, GoldR7, GoldR8, GoldTotal);
+        }
 
-            SilverR6Pre.Value = SilverDroprate.R6GoldPrefix;
-            SilverR6Post.Value = SilverDroprate.R6GoldPostfix;
-            SilverR7.Value = SilverDroprate.R7;
-            SilverR8.Value = SilverDroprate.R8;
-            SilverTotal.Text = $"{(SilverDroprate.R6GoldPrefix + SilverDroprate.R6GoldPostfix + SilverDroprate.R7 + SilverDroprate.R8) * 100}%";
+        private void Render(Droprate droprate, DecimalUpDown r6Pre, DecimalUpDown r6Post, DecimalUpDown r7, DecimalUpDown r8, TextBlock total)
+        {
+            r6Pre.Value = droprate.R6GoldPrefix;
+            r6Post.Value = droprate.R6GoldPostfix;
+            r7.Value = droprate.R7;
+            r8.Value = droprate.R8;
 
-            GoldR6Pre.Value = GoldDroprate.R6GoldPrefix;
-            GoldR6Post.Value = GoldDroprate.R6GoldPostfix;
-            GoldR7.Value = GoldDroprate.R7;
-            GoldR8.Value = GoldDroprate.R8;
-            GoldTotal.Text = $"{(GoldDroprate.R6GoldPrefix + GoldDroprate.R6GoldPostfix + GoldDroprate.R7 + GoldDroprate.R8) * 100}%";
+            RenderTotal(droprate, total);
+        }
 
-            //TODO: Add over 100% error
+        private void RenderTotal(Droprate droprate, TextBlock total)
+        {
+            total.Text = $"{droprate.TotalPercentage}%";
+            total.Foreground = !droprate.Valid ? Brushes.Red : Brushes.Black;
         }
 
         private void LargerThan100(string item) => MessageBox.Show($"{item} is larger than 100%", "Error");
@@ -82,29 +89,17 @@ namespace MhwKtDroprateEditor
 
         private void LoadHex(object sender, RoutedEventArgs e) => PresetDroprate("Hex.json");
 
-        private void BrownChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        private void TextBoxChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
         {
-            if (!rendered) return;
+            if (!Rendered) return;
             if (((FrameworkElement)e.Source).Name == "BrownR6Pre") BrownDroprate.R6GoldPrefix = BrownR6Pre?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "BrownR6Post") BrownDroprate.R6GoldPostfix = BrownR6Post?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "BrownR7") BrownDroprate.R7 = BrownR7?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "BrownR8") BrownDroprate.R8 = BrownR8?.Value ?? 0;
-            Render();
-        }
-
-        private void SilverChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-            if (!rendered) return;
             if (((FrameworkElement)e.Source).Name == "SilverR6Pre") SilverDroprate.R6GoldPrefix = SilverR6Pre?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "SilverR6Post") SilverDroprate.R6GoldPostfix = SilverR6Post?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "SilverR7") SilverDroprate.R7 = SilverR7?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "SilverR8") SilverDroprate.R8 = SilverR8?.Value ?? 0;
-            Render();
-        }
-
-        private void GoldChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
-        {
-            if (!rendered) return;
             if (((FrameworkElement)e.Source).Name == "GoldR6Pre") GoldDroprate.R6GoldPrefix = GoldR6Pre?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "GoldR6Post") GoldDroprate.R6GoldPostfix = GoldR6Post?.Value ?? 0;
             if (((FrameworkElement)e.Source).Name == "GoldR7") GoldDroprate.R7 = GoldR7?.Value ?? 0;
@@ -125,15 +120,15 @@ namespace MhwKtDroprateEditor
             if (dlg.ShowDialog() == true)
             {
                 var filename = dlg.FileName;
-                var input = System.IO.File.ReadAllBytes(filename);
+                var input = File.ReadAllBytes(filename);
                 var buffer = new byte[1];
                 for (var i = 6; i < input.Length - 1; i += 24)
                 {
                     buffer[0] = input[i + 8];
-                    var r6Pre = BitConverter.ToInt32(input, i + 8);
-                    var r6Post = BitConverter.ToInt32(input, i + 12);
-                    var r7 = BitConverter.ToInt32(input, i + 16);
-                    var r8 = BitConverter.ToInt32(input, i + 20);
+                    var r6Pre = BitConverter.ToInt32(input, i + 8) / 100;
+                    var r6Post = BitConverter.ToInt32(input, i + 12) / 100;
+                    var r7 = BitConverter.ToInt32(input, i + 16) / 100;
+                    var r8 = BitConverter.ToInt32(input, i + 20) / 100;
                     if (i == 6)
                         BrownDroprate = new Droprate(WeaponType.Dissolved, r6Pre, r6Post, r7, r8);
                     else if (i == 30)
@@ -148,8 +143,23 @@ namespace MhwKtDroprateEditor
 
         private async void SaveFile(object sender, RoutedEventArgs e)
         {
-            System.IO.Stream fs;
-            var dlg = new Microsoft.Win32.SaveFileDialog()
+            if (!BrownDroprate.Valid)
+            {
+                LargerThan100(BrownDroprate.Type.ToString());
+                return;
+            }
+            if (!SilverDroprate.Valid)
+            {
+                LargerThan100(SilverDroprate.Type.ToString());
+                return;
+            }
+            if (!GoldDroprate.Valid)
+            {
+                LargerThan100(GoldDroprate.Type.ToString());
+                return;
+            }
+
+            var dlg = new Microsoft.Win32.SaveFileDialog
             {
                 FileName = "em117_grade_lot",
                 DefaultExt = ".em117glt",
@@ -160,23 +170,29 @@ namespace MhwKtDroprateEditor
 
             if (dlg.ShowDialog() == true)
             {
-                if ((fs = dlg.OpenFile()) != null)
+                using (var fs = dlg.OpenFile())
                 {
-                    //var header = new byte[] { 0x18, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-                    //var buffer = new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-                    //var items = header.ToList();
-                    //foreach (var item in listboxout)
-                    //{
-                    //    var hex = item.Key.Substring(4);
-                    //    items.Add(Convert.ToByte(int.Parse(hex.Substring(2), System.Globalization.NumberStyles.HexNumber)));
-                    //    items.Add(Convert.ToByte(int.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber)));
-                    //    items.AddRange(buffer);
-                    //}
-                    //var output = items.ToArray();
-                    //await fs.WriteAsync(output, 0, output.Length);
-                    //fs.Close();
+                    var buffer = new List<byte> { 0x1F, 0x00, 0x03, 0x00, 0x00, 0x00 };
+                    var brownHeader = new List<byte> { 0x00, 0x00, 0x00, 0x00, 0xA8, 0x03, 0x00, 0x00 };
+                    var silverHeader = new List<byte> { 0x01, 0x00, 0x00, 0x00, 0xA9, 0x03, 0x00, 0x00 };
+                    var goldHeader = new List<byte> { 0x02, 0x00, 0x00, 0x00, 0xAA, 0x03, 0x00, 0x00 };
+
+                    buffer.AddRange(brownHeader);
+                    buffer.AddRange(BrownDroprate.ToByte);
+                    buffer.AddRange(silverHeader);
+                    buffer.AddRange(SilverDroprate.ToByte);
+                    buffer.AddRange(goldHeader);
+                    buffer.AddRange(GoldDroprate.ToByte);
+
+                    var output = buffer.ToArray();
+                    await fs.WriteAsync(output, 0, output.Length);
+                    fs.Close();
                 }
             }
         }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- MHW-KT-Droprate-Editor/Models/Droprate.cs
+++ MHW-KT-Droprate-Editor/Models/Droprate.cs
@@ -1,4 +1,7 @@
-﻿namespace MhwKtDroprateEditor.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace MhwKtDroprateEditor.Models
 {
     public class Droprate
     {
@@ -8,12 +11,22 @@
         public decimal R7 { get; set; }
         public decimal R8 { get; set; }
 
-        public Droprate()
-        {
-        }
+        public int TotalPercentage => (int) ((R6GoldPrefix + R6GoldPostfix + R7 + R8) * 100);
+        public bool Valid => TotalPercentage == 100;
 
-        public Droprate(WeaponType type)
-            => Type = type;
+        public byte[] ToByte 
+            => new List<byte>
+            {
+                (byte) (R6GoldPrefix * 100), 0x00, 0x00, 0x00,
+                (byte) (R6GoldPostfix * 100), 0x00, 0x00, 0x00,
+                (byte) (R7 * 100), 0x00, 0x00, 0x00,
+                (byte) (R8 * 100), 0x00, 0x00, 0x00,
+            }.ToArray();
+
+        // JsonConvert need this
+        public Droprate() { }
+
+        public Droprate(WeaponType type) => Type = type;
 
         public Droprate(WeaponType type, decimal r6Pre, decimal r6Post, decimal r7, decimal r8)
         {
